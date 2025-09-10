01684d9098395ebc7ffe6161d4c02900e69d4a1d
Felipe Mammoli <fmammoli@gmail.com>
2025-09-10 16:58:32 -0300

    response message

--- app/api/zap/route.ts
+++ app/api/zap/route.ts
@@ -10,6 +10,7 @@ export async function GET(req: NextRequest) {
   console.log(token);
   if (mode === "subscribe" && token === verifyToken) {
     console.log("WEBHOOK VERIFIED");
+
     return new NextResponse(challenge, { status: 200 });
   } else {
     return new NextResponse("Forbidden", { status: 403 });
@@ -23,6 +24,58 @@ export async function POST(req: NextRequest) {
   console.log(`\n\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(body, null, 2));
 
+  try {
+    const from = body.entry?.[0]?.changes?.[0]?.value?.messages?.[0]?.from;
+
+    if (from) {
+      console.log("Message sent from number:", from);
+      sendMessage(from);
+    } else {
+      console.log("No 'from' field found in the message.");
+    }
+  } catch (err) {
+    console.error("Error extracting sender number:", err);
+  }
+
   // Always respond 200 so WhatsApp knows delivery worked
   return new NextResponse(null, { status: 200 });
+}
+
+async function sendMessage(to: string) {
+  const token = process.env.WHATSAPP_TOKEN; // set in .env.local
+  const phoneNumberId = process.env.WHATSAPP_PHONE_NUMBER_ID; // set in .env.local
+
+  const bodyText =
+    "Obrigado pela mensagem, entre no link: https://whatsapp-test-six.vercel.app";
+
+  try {
+    const response = await fetch(
+      `https://graph.facebook.com/v23.0/${phoneNumberId}/messages`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          messaging_product: "whatsapp",
+          recipient_type: "individual",
+          to,
+          type: "text",
+          text: {
+            preview_url: true,
+            body: bodyText,
+          },
+        }),
+      }
+    );
+
+    const result = await response.json();
+    console.log("WhatsApp API response:", result);
+
+    return NextResponse.json({ success: true, result });
+  } catch (err) {
+    console.error("Error sending WhatsApp message:", err);
+    return NextResponse.json({ success: false, error: err }, { status: 500 });
+  }
 }
